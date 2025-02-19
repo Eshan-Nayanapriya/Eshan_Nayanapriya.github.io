66976bba967505ff6d768e9463ef69d70d2d7554
Eshan Nayanapriya <eshannayanapriyaprofessional@gmail.com>
2025-02-19 20:12:03 +0530

    Implement responsive Navbar component with toggle functionality; update styles and add menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { MENU_LINKS } from "../utils/data";
+import { Link } from "react-scroll";
+//import LOGO from '../assets/images/LOGO.png';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(true);
 
-export default Navbar
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="container mx-auto sticky top-5 z-10">
+      <div className="flex items-center justify-between rounded-full bg-white/25 border border-[#fee6cc] backfrop-blur-[10px] m-5 p-3 md:p-0">
+        {/*Logo*/}
+        <img src="" alt="Logo" className="h-7 ml-6 -mb-1" />
+
+        {/*Navigation Links*/}
+        <ul
+          className={`
+          ${isOpen ? "flex" : "hidden"} 
+          menu-wrapper`}
+        >
+          {MENU_LINKS.map((item) => (
+            <li key={item.id}>
+              <Link
+                activeClass="active"
+                to={item.to}
+                smooth
+                spy
+                offset={item.offset}
+                className="menu-item"
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        {/*Hire Me Button*/}
+        <button className="">Hire Me</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
